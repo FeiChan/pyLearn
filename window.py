fe1d39a88fc01aea212aff103df933a894339121
cf <cf@pornhub.gov>
2018-03-09 21:10:36 +0800

    third commit

---
+++ window.py
@@ -0,0 +1,17 @@
+import pygame
+
+def main():
+    
+	screen=pygame.display.set_mode((480,852),0,32)
+
+	background=pygame.image.load("./")
+
+	while True:
+
+	   screen.blit(background(),(0,0))
+
+	   pygame.display.update()
+
+if __name__=="__main__":
+
+    main()
